dff28497734833b926abd4af478948a2c047cea7
wade3hamati <wade3_hamati@outlook.com>
2025-09-04 13:52:44 -0400

    fifth  challenge - attempt 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,120 +16,174 @@ import java.util.zip.InflaterInputStream;
 public class Main {
   public static void main(String[] args) throws IOException {
 
-     final String firstCommand = args[0];
+    final String firstCommand = args[0];
 
-     switch (firstCommand) {
-       case "init" -> {
-         final File root = new File(".git");
-         new File(root, "objects").mkdirs();
-         new File(root, "refs").mkdirs();
-         final File head = new File(root, "HEAD");
+    switch (firstCommand) {
+     case "init" -> {
+       final File root = new File(".git");
+       new File(root, "objects").mkdirs();
+       new File(root, "refs").mkdirs();
+       final File head = new File(root, "HEAD");
 
-         try {
-           head.createNewFile();
-           Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
-           System.out.println("Initialized git directory");
-         } catch (IOException e) {
-           throw new RuntimeException(e);
-         }
+       try {
+         head.createNewFile();
+         Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
+         System.out.println("Initialized git directory");
+       } catch (IOException e) {
+         throw new RuntimeException(e);
        }
-       case "cat-file" -> {
-         switch(args[1]){
-           case "-p" -> {
-               String objectHash = args[2];
-
-               String dir = "./.git/objects/" + objectHash.substring(0, 2) + "/";
-
-               File compressedFilePath = new File(dir + objectHash.substring(2));
-
-               StringBuilder content = new StringBuilder();
-
-               try (FileInputStream fis = new FileInputStream(compressedFilePath);
-                    InflaterInputStream iis = new InflaterInputStream(fis);
-                    InputStreamReader isr = new InputStreamReader(iis);
-                    BufferedReader br = new BufferedReader(isr)) {
-
-                 String line;
-                 while ((line = br.readLine()) != null) {
-                   content.append(line);
-                 }
-                 System.out.print(content.substring(content.indexOf("\0") + 1));
-
-               } catch (IOException e) {
-                 throw new RuntimeException(e);
-               }
-           }
-         }
-       }
-       case "hash-object" -> {
-         switch(args[1]){
-           case "-w" -> {
-
-             String fileName = args[2];
-             File file = new File(fileName);
-
-             System.out.print(createBlob(file));
-           }
-         }
-       }
-       case "ls-tree" -> {
-
-         String objectHash = args[2];
-
-         String dir = "./.git/objects/" + objectHash.substring(0, 2) + "/";
-         File compressedFilePath = new File(dir + objectHash.substring(2));
-
-         try (FileInputStream fis = new FileInputStream(compressedFilePath);
-              InflaterInputStream iis = new InflaterInputStream(fis)) {
-
-           byte[] decompressed = iis.readAllBytes();
-
-           int nullIndex = 0;
-           while (decompressed[nullIndex] != 0) {
-             nullIndex++;
-           }
-
-           int i = nullIndex + 1;
-
-           while ( i < decompressed.length) {
-             int spaceIndex = i;
-             while(decompressed[spaceIndex] != ' '){
-               spaceIndex++;
-             }
-             String mode = new String(decompressed, i, spaceIndex - i);
-
-             i = spaceIndex + 1;
-             int nullTerminator = i;
-             while(decompressed[nullTerminator] != 0){
-               nullTerminator++;
-             }
-             String filename = new String(decompressed, i, nullTerminator - i);
-             i = nullTerminator + 1;
-
-             byte[] shaBytes = Arrays.copyOfRange(decompressed, i, i + 20);
-             StringBuilder shaHex = new StringBuilder();
-             for (byte b : shaBytes) {
-               shaHex.append(String.format("%02x", b));
-             }
-             i += 20;
-
-             System.out.printf("%s%n", filename);
-           }
-
-         } catch (IOException e) {
-           throw new RuntimeException(e);
-         }
-       }
-       case "write-tree" -> {
-         Path dir = Paths.get("./");
-         String treeHash = createTree(dir.toFile());
-         System.out.println(treeHash);
-       }
-       default -> System.out.println("Unknown Command: " + firstCommand);
      }
+     case "cat-file" -> {
+       switch(args[1]){
+         case "-p" -> {
+             String objectHash = args[2];
+
+             String dir = "./.git/objects/" + objectHash.substring(0, 2) + "/";
+
+             File compressedFilePath = new File(dir + objectHash.substring(2));
+
+             StringBuilder content = new StringBuilder();
+
+             try (FileInputStream fis = new FileInputStream(compressedFilePath);
+                  InflaterInputStream iis = new InflaterInputStream(fis);
+                  InputStreamReader isr = new InputStreamReader(iis);
+                  BufferedReader br = new BufferedReader(isr)) {
+
+               String line;
+               while ((line = br.readLine()) != null) {
+                 content.append(line);
+               }
+               System.out.print(content.substring(content.indexOf("\0") + 1));
+
+             } catch (IOException e) {
+               throw new RuntimeException(e);
+             }
+         }
+       }
+     }
+     case "hash-object" -> {
+       switch(args[1]){
+         case "-w" -> {
+
+           String fileName = args[2];
+           File file = new File(fileName);
+
+           System.out.print(createBlob(file));
+         }
+       }
+     }
+     case "ls-tree" -> {
+
+       String objectHash = args[2];
+
+       String dir = "./.git/objects/" + objectHash.substring(0, 2) + "/";
+       File compressedFilePath = new File(dir + objectHash.substring(2));
+
+       try (FileInputStream fis = new FileInputStream(compressedFilePath);
+            InflaterInputStream iis = new InflaterInputStream(fis)) {
+
+         byte[] decompressed = iis.readAllBytes();
+
+         int nullIndex = 0;
+         while (decompressed[nullIndex] != 0) {
+           nullIndex++;
+         }
+
+         int i = nullIndex + 1;
+
+         while ( i < decompressed.length) {
+           int spaceIndex = i;
+           while(decompressed[spaceIndex] != ' '){
+             spaceIndex++;
+           }
+           String mode = new String(decompressed, i, spaceIndex - i);
+
+           i = spaceIndex + 1;
+           int nullTerminator = i;
+           while(decompressed[nullTerminator] != 0){
+             nullTerminator++;
+           }
+           String filename = new String(decompressed, i, nullTerminator - i);
+           i = nullTerminator + 1;
+
+           byte[] shaBytes = Arrays.copyOfRange(decompressed, i, i + 20);
+           StringBuilder shaHex = new StringBuilder();
+           for (byte b : shaBytes) {
+             shaHex.append(String.format("%02x", b));
+           }
+           i += 20;
+
+           System.out.printf("%s%n", filename);
+         }
+
+       } catch (IOException e) {
+         throw new RuntimeException(e);
+       }
+     }
+     case "write-tree" -> {
+       Path dir = Paths.get("./");
+       String treeHash = createTree(dir.toFile());
+       System.out.println(treeHash);
+     }
+     case "commit-tree" -> {
+       switch(args[2]){
+         case "-p" -> {
+           String treeSha = args[1];
+           String commitSha = args[3];
+           switch(args[4]){
+             case "-p" -> {
+               String commitMessage = args[5].substring(1,-2);
+               byte[] commitMessageBytes = commitMessage.getBytes();
+
+               String treeEntry = "tree " + treeSha + "\0";
+               byte[] treeEntryBytes = treeEntry.getBytes();
+
+               String authorEntry = "author Wadeh Hamati <wade3_hamati@outlook.com> 1243040974 -0700\0";
+               byte[] authorEntryBytes = authorEntry.getBytes();
+
+               String committerEntry = "author Wadeh Hamati <wade3_hamati@outlook.com> 1243040974 -0700\0";
+               byte[] committerEntryBytes = committerEntry.getBytes();
+
+               byte[] content = new byte[
+                       treeEntryBytes.length +
+                       authorEntryBytes.length +
+                       committerEntryBytes.length +
+                       commitMessageBytes.length];
+
+               System.arraycopy(treeEntryBytes, 0, content, 0, treeEntryBytes.length);
+               System.arraycopy(authorEntryBytes, 0, content, treeEntryBytes.length, authorEntryBytes.length);
+               System.arraycopy(committerEntryBytes, 0, content, treeEntryBytes.length + authorEntryBytes.length, committerEntryBytes.length);
+               System.arraycopy(commitMessageBytes, 0, content, treeEntryBytes.length + authorEntryBytes.length + committerEntryBytes.length, commitMessageBytes.length);
+
+               String commitHash = getObjectHash40(content);
+
+               String parentDir = "./.git/objects/" + commitHash.substring(0, 2);
+               String filePath = parentDir + "/" + commitHash.substring(2);
+
+               File dir = new File(parentDir);
+               if (!dir.exists() && !dir.mkdirs()) {
+                 throw new IOException("Failed to create directory: " + parentDir);
+               }
+
+               File objectFile = new File(filePath);
+               if (objectFile.exists()) {
+                 System.out.println(commitHash);
+               }
+               try (FileOutputStream fos = new FileOutputStream(objectFile);
+                    DeflaterOutputStream dos = new DeflaterOutputStream(fos)) {
+                 dos.write(content);
+               }
+               System.out.println(commitHash);
+             }
+           }
+         }
+       }
+     }
+     default -> System.out.println("Unknown Command: " + firstCommand);
+    }
   }
 
-  public static String getObjectHash40(byte[] input){
+  public static String getObjectHash40( byte[] input){
     try{
       MessageDigest md = MessageDigest.getInstance("SHA-1");
 
